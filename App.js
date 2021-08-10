0c0a6869b6a88fd3e707aafbf7411b98535270f4
unknown <martugone06@hotmail.com>
2021-08-10 18:33:46 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+
+import store from './store'
+import { init } from './db';
+
+init()
+  .then(() => console.log('Database initialized'))
+  .catch((err) => {
+    console.log('Database failed to connect');
+    console.log(err.message)
+  });
+
+// navigation
+import MainNavigator from './navigation'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<Provider store={store}><MainNavigator /></Provider>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constants/Map.js
@@ -0,0 +1,3 @@
+export default {
+    API_KEY: 'AIzaSyBiXRB5N185FJTxH6DX4t78Mctd9JF8zOA',
+  }
